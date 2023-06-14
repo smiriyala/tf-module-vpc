8cb539c859fdf6118d362dc863280b9fe6d3c92b
subbum <subbu@gmail.com>
2023-06-14 22:44:59 +0100

    create NAT GW, ElasticIP, ADD route to private subnet

--- main.tf
+++ main.tf
@@ -68,6 +68,13 @@ resource "aws_route_table" "public-route-table" {
 resource "aws_route_table" "private-route-table" {
   vpc_id   = aws_vpc.main.id
   for_each = var.private_subnets
+
+  #IN Part of STEP 5.3, NAT Gateway routing adding here. 
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id  = aws_nat_gateway.nat-gateways["public-${split("-", each.value["name"])[1]}"].id
+  }
+
   tags = merge(
     var.tags,
     { Name = "${var.env}-${each.value["name"]}" }
@@ -109,3 +116,27 @@ resource "aws_internet_gateway" "internetGateWay" {
   )
 }
 
+# STEP 6.1 CREATE A NAT GATEWAY Which is part of the PUBLIC SUBNET
+resource "aws_nat_gateway" "nat-gateways" {
+
+  # need to create two NAT gateways for each foe public subnets
+  for_each = var.public_subnets
+  allocation_id = aws_eip.nat[each.value["name"]].id
+  subnet_id     = aws_subnet.public_subnets[each.value["name"]].id
+
+  tags = merge(
+    var.tags,
+    { Name = "${var.env}-${each.value["name"]}" }
+  )
+}
+
+# IN Part of STEP6.2, ELASTIC IP Address and 
+resource "aws_eip" "nat" {
+  for_each = var.public_subnets
+  vpc = true
+}
+
+# STEP 6.3 - ADD route in private subnet.
+  # IN Part of STEP6.3, ELASTIC IP Address, lets create
+  # ONCE NAT GATEWAY CREATED, route table  must be added to private route table
+  # UPDATED IN STEP 3.2: for private route table wiht matching availability zone. 
