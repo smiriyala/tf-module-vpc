d4e2f843f2dedec6836cee6382a3ea5d12073529
subbum <subbu@gmail.com>
2023-06-16 17:21:19 +0100

    added subnet out variables

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,8 @@
 output "public_subnets" {
     value = aws_subnet.public_subnets
 }
+
+## This output needed by docdb to crate subnets-group
+output "private_subnets" {
+    value = aws_subnet.private_subnets
+}
