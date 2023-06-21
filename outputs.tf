7e8d1de03437f75b174dc2687c460851581b0fd9
subbum <subbu@gmail.com>
2023-06-21 15:08:35 +0100

    output added from VPC to be used in main module

--- outputs.tf
+++ outputs.tf
@@ -6,3 +6,8 @@ output "public_subnets" {
 output "private_subnets" {
     value = aws_subnet.private_subnets
 }
+
+## This output returned VPC ID to be used in App Module to create Security Group.
+output "vpc_id" {
+    value = aws_vpc.main.id
+}
